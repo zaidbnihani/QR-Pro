226bf2c38ca08fbc72719f6c4e144366408b6d2e
zaidmtsmbanihani <zaidbnihani73@gmail.com>
2026-08-22 15:24:59 +0300

    chore: bump version to 1.1.2 and update app icons
    - Update app version to 1.1.2
    - Update app launcher icons to use drawable resources
    - Improve CI build script reliability for keystore and artifact naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.qrgenerator.yhqmzo"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.1.1"
+    versionCode = 4
+    versionName = "1.1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
